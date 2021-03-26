3d660c5773b7384422eb70eb41503f7f6f7232ec
Nomentsafidy <mnomentsafidy@gmail.com>
2021-03-26 09:36:32 +0000

    feat constraint: remove constraint

--- src/app/pages/plgraphe/plgraphe.page.ts
+++ src/app/pages/plgraphe/plgraphe.page.ts
@@ -1,35 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ContrainteService } from 'src/app/services/';
 @Component({
-  selector: 'app-plgraphe',
-  templateUrl: './plgraphe.page.html',
-  styleUrls: ['./plgraphe.page.scss'],
+    selector: 'app-plgraphe',
+    templateUrl: './plgraphe.page.html',
+    styleUrls: ['./plgraphe.page.scss'],
 })
 export class PlgraphePage implements OnInit {
-  public var1: string;
-  public var2: string;
+    public var1: string;
+    public var2: string;
 
-  public contraintes: string[] = ['zaza'];
+    public contraintes: {
+        id: number;
+        contrainte: string;
+    }[] = [
+        {
+            id: 0,
+            contrainte: '',
+        },
+    ];
 
-  constructor(private contrainteService: ContrainteService) {}
+    constructor(private contrainteService: ContrainteService) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  public contrainteChange(event: any, index: number) {
-    console.log('contrainte i', index);
-    this.contraintes[index] = event.target.value;
-    console.log(this.contraintes[index]);
-    // const contrainte: string = event.target.value;
-    // if (this.contrainteService.isContrainte(contrainte)) {
-    //   if (this.contraintes.length <= index) {
-    //     this.contraintes.push();
-    //   }
-    // }
-  }
+    public contrainteChange(event: any, index: number) {
+        console.log('contraintes', this.contraintes);
 
-  private rebuildContraintes() {}
+        // console.log('contrainte i', index);
+        // this.contraintes[index] = event.target.value;
+        // console.log(this.contraintes[index]);
+        // const contrainte: string = event.target.value;
+        // if (this.contrainteService.isContrainte(contrainte)) {
+        //   if (this.contraintes.length <= index) {
+        //     this.contraintes.push();
+        //   }
+        // }
+    }
 
-  public addContraintInput() {
-    this.contraintes.push('');
-  }
+    private rebuildContraintes() {}
+
+    public addContraintInput() {
+        this.contraintes.push({
+            id: this.contraintes.length,
+            contrainte: '',
+        });
+    }
+
+    public removeConstraint(id: number) {
+        let tmp = this.contraintes.filter((constrainte) => {
+            return constrainte.id !== id ? constrainte : null;
+        });
+        this.contraintes = tmp.filter((constrainte, id) => {
+            constrainte.id = id;
+            return constrainte;
+        });
+    }
 }
--- src/app/services/contrainte.service.ts
+++ src/app/services/contrainte.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ContrainteService {
-  constructor() {}
+    private contrainteReg: RegExp;
+    private arithmeticOper: string[] = ['+', '-', '*', '/'];
+    private comparisonOper: string[] = ['+', '-', '*', '/'];
 
-  public isContrainte(contrainte: string): boolean {
-    if (contrainte) {
-      return true;
-    } else {
-      return false;
+    constructor() {}
+
+    public isContrainte(var1: string, var2: string, contrainte: string): boolean {
+        // let tmp = /^(+|-){0,1}[0-9]* $/;
+        let tmp1 = new RegExp(`^(+|-){0,1}[0-9]*${var1}(+|-|*|/){1,1}${var2}(=|<=|>=){1,1}[0-9]{1,}$`, 'i');
+        console.log('contrainte', contrainte);
+
+        if (tmp1.test(contrainte)) {
+            console.log(true);
+
+            return true;
+        } else {
+            console.log(false);
+
+            return false;
+        }
     }
-  }
 }
